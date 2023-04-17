1077a801d45a38547fa1a86a5c1291445d3ee2eb
Jeffrey Chaussé <89852717+jeffreychausse@users.noreply.github.com>
2023-04-17 13:23:59 -0400

    Add files via upload

--- cc_tools.ps1
+++ cc_tools.ps1
@@ -35,7 +35,7 @@ function Remove-Devices {
     $Collection = Get-MdbcCollection -Name devices -Database $Database
 
     # Removes all devices
-    if ($All -eq $true) {
+    if ($All) {
         $result = Remove-MdbcData -Collection $Collection -Filter @{} -Many -Result # Sends an empty document to match all existing documents
     }
 
@@ -56,12 +56,51 @@ function Remove-Devices {
 
 }
 
+function Remove-Taskflows {
+
+    # Requires one of the following parameters
+    param(
+    [switch]$All,
+    [string]$StartingWith,
+    [string]$Containing
+    )
+
+    # Connect the client
+    Connect-Mdbc -ConnectionString "mongodb://$($ccIp):27055"
+
+    # Then set the database/collection we want to work on
+    $Database = Get-MdbcDatabase sc-datastore
+    $Collection = Get-MdbcCollection -Name taskflows -Database $Database
+
+    # Removes all taskflows
+    if ($All) {
+        $result = Remove-MdbcData -Collection $Collection -Filter @{} -Many -Result # Sends an empty document to match all existing documents
+    }
+
+    # Removes all taskflows starting with...
+    if ($StartingWith) {
+        $regex = "^" + $StartingWith
+        $result = Remove-MdbcData -Collection $Collection -Many -Result -Filter @{displayName = @{'$in' = @([regex]$regex)}} # $in is a MongoDB expression
+    }
+
+    # Removes all taskflows that contains...
+    if ($Containing) {
+        $result = Remove-MdbcData -Collection $Collection -Many -Result -Filter @{displayName = @{'$in' = @([regex]$Containing)}} # $in is a MongoDB expression
+    }
+
+    if ($result.IsAcknowledged) {
+        Write-Host "$($result.DeletedCount) taskflows have been deleted"
+    }
+
+}
+
 #########################################
 ############# Function Call #############
 #########################################
 
-#Remove-Devices -StartingWith BND-VD
-Remove-Devices -All
+#Remove-Devices -Containing BND
+#Remove-Devices -All
+Remove-Taskflows -All
 
 #################################
 ########## End Of File ##########
