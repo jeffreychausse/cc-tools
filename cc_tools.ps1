308d47eabcec6fe9a20818279d13ebc7ddcb800c
Jeffrey Chaussé <89852717+jeffreychausse@users.noreply.github.com>
2023-06-08 10:59:54 -0400

    Add files via upload
    Add of Update-Prefix function

--- cc_tools.ps1
+++ cc_tools.ps1
@@ -11,7 +11,7 @@ Import-Module Mdbc # https://github.com/nightroman/Mdbc
 ########## Variable Declaration ##########
 ##########################################
 
-$ccIp = "172.29.222.237" # Enter the Control Center IP Address
+$ccIp = "172.26.156.86" # Enter the Control Center IP Address
 #$ProjectId = "bnd" # Enter the project identifier
 
 #########################################
@@ -94,13 +94,48 @@ function Remove-Taskflows {
 
 }
 
+function Update-Prefix {
+    
+    # Requires 2 parameters. The first param is the old prefix and the second param will be the new prefix.
+    if ($args.Length -ne 2) {throw "Invalid parameter count. Two parameters are required."}
+
+    $oldPrefix = $args[0]
+    $newPrefix = $args[1]
+
+    $regex = "^" + $oldPrefix
+
+    # Connect the client
+    Connect-Mdbc -ConnectionString "mongodb://$($ccIp):27055"
+
+    # Then set the database/collection we want to work on
+    $Database = Get-MdbcDatabase sc-datastore
+    $Collection = Get-MdbcCollection -Name devices -Database $Database
+
+    $document = Get-MdbcData -Filter @{deviceName = @{'$in' = @([regex]$regex)}} -Collection $Collection -First 1
+
+    if ($null -eq $document) {throw "No device name is matching the provided prefix."}
+
+    $i = 0
+
+    while ($null -ne $document) {
+        Get-MdbcData -Filter @{_id = $document._id} -Update @{'$set' = @{deviceName = $document.deviceName.Replace($oldPrefix, $newPrefix)}} -Collection $Collection | Out-Null
+        
+        $document = Get-MdbcData -Filter @{deviceName = @{'$in' = @([regex]$regex)}} -Collection $Collection -First 1
+        $i++
+    }
+
+    Write-Host $i "device names have been updated."
+
+}
+
 #########################################
 ############# Function Call #############
 #########################################
 
-#Remove-Devices -Containing BND
+#Remove-Devices -Containing INT0
 #Remove-Devices -All
-Remove-Taskflows -All
+#Remove-Taskflows -All
+Update-Prefix HUX ATI
 
 #################################
 ########## End Of File ##########
